3ba733d5d6fc7b092eb8e2431fd01260290813aa
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-12-27 19:47:10 -0300

    Logo and download button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,13 @@
 import { Parallax } from "react-scroll-parallax"
 import Lenis from '@studio-freight/lenis'
 import { useEffect } from 'react'
+import 'animate.css';
+
+
+import sonicMegamixLogo from "../assets/logo.png"
 
  const Home = () => {
+  {/* Lenis (smooth mouse scroll) configuration */}
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.1,
@@ -25,15 +30,36 @@ import { useEffect } from 'react'
 
   return (
     <div className=" m-auto">
-      {/* Hill and water backgrounds */}
-      <div className="h-screen">
-        <div className="relative w-full h-screen bg-fixed overflow-hidden bg-[#1938cf]">
-          <div className="backgroundLayer bg-[url(/BG_1.png)] scrolling-background-1"/>
-          <div className="backgroundLayer bg-[url(/BG_2.png)]"/>
-          <div className="backgroundLayer bg-[url(/BG_3.png)]"/>
-          <div className="backgroundLayer bg-[url(/BG_4.png)] scrolling-background-2"/>
-          <div className="backgroundLayer bg-[url(/BG_5.png)] scrolling-background-3"/>
-          <div className="backgroundLayer bg-[url(/BG_6.png)] scrolling-background-4"/>
+      <div className="relative w-full h-screen bg-fixed overflow-hidden bg-[#1938cf]">
+        {/* Hill and water backgrounds */}
+        <div className="backgroundLayer bg-[url(/BG_1.png)] scrolling-background-1"/>
+        <div className="backgroundLayer bg-[url(/BG_2.png)]"/>
+        <div className="backgroundLayer bg-[url(/BG_3.png)]"/>
+        <div className="backgroundLayer bg-[url(/BG_4.png)] scrolling-background-2"/>
+        <div className="backgroundLayer bg-[url(/BG_5.png)] scrolling-background-3"/>
+        <div className="backgroundLayer bg-[url(/BG_6.png)] scrolling-background-4"/>
+
+        
+        <div className="relative w-full h-full flex flex-col flex-wrap justify-center content-center items-start">
+          {/* Logo */}
+          <img className="w-[550px] 2xl:w-[880px] animate__animated animate__fadeInDown" src={sonicMegamixLogo} />
+
+          {/* Download button */}
+          <div className="group mt-auto mb-20 ml-auto mr-auto font-bbh
+                          text-white text-shadow-[0_6px_0px_rgb(0_0_0/_0.25)]">
+            <div className="p-1.5 rounded-4xl group-hover:rounded-2xl transition-all duration-300
+                            bg-[#20327b] drop-shadow-[7px_7px_0px_rgba(1,1,1,1)]">
+              <div className="p-1.5 rounded-4xl group-hover:rounded-2xl transition-all duration-300
+                              bg-linear-to-t from-[#203582] via-[#8388cc] to-[#d0cfed]">
+                <div className="text-3xl 2xl:text-6xl p-5
+                                rounded-4xl group-hover:rounded-2xl transition-all duration-300 hover:cursor-pointer
+                                bg-linear-to-t from-[#d0cfed] via-[#8388cc] to-[#203582]">
+                  DOWNLOAD
+                </div>
+              </div>
+            </div>
+          </div>
+          
         </div>
       </div>
 
