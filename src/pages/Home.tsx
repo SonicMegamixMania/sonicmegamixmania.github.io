b6ce297169ab7cc06b9fefdd298ca9896a567ec7
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-12-27 15:29:31 -0300

    Homepage CSS refactoring

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,41 +28,41 @@ import { useEffect } from 'react'
       {/* Hill and water backgrounds */}
       <div className="h-screen">
         <div className="relative w-full h-screen bg-fixed overflow-hidden bg-[#1938cf]">
-          <div className="absolute inset-0 bg-[url(/BG_1.png)] bg-size-[1500px_auto] bg-bottom bg-repeat-x scrolling-background-1"></div>
-          <div className="absolute inset-0 bg-[url(/BG_2.png)] bg-size-[1500px_auto] bg-bottom bg-no-repeat"></div>
-          <div className="absolute inset-0 bg-[url(/BG_3.png)] bg-size-[1500px_auto] bg-bottom bg-repeat-x"></div>
-          <div className="absolute inset-0 bg-[url(/BG_4.png)] bg-size-[1500px_auto] bg-bottom bg-repeat-x scrolling-background-2"></div>
-          <div className="absolute inset-0 bg-[url(/BG_5.png)] bg-size-[1500px_auto] bg-bottom bg-repeat-x scrolling-background-3"></div>
-          <div className="absolute inset-0 bg-[url(/BG_6.png)] bg-size-[1500px_auto] bg-bottom bg-repeat-x scrolling-background-4"></div>
+          <div className="backgroundLayer bg-[url(/BG_1.png)] scrolling-background-1"/>
+          <div className="backgroundLayer bg-[url(/BG_2.png)]"/>
+          <div className="backgroundLayer bg-[url(/BG_3.png)]"/>
+          <div className="backgroundLayer bg-[url(/BG_4.png)] scrolling-background-2"/>
+          <div className="backgroundLayer bg-[url(/BG_5.png)] scrolling-background-3"/>
+          <div className="backgroundLayer bg-[url(/BG_6.png)] scrolling-background-4"/>
         </div>
       </div>
 
       {/* Blue info section */}
       <div className="relative h-screen bg-[#2b60c9]">
         {/* Scrolling bar */}
-        <div className="-translate-y-[23px] w-full h-12 bg-[url(/BARS.png)] bg-size-[1500px_auto] bg-repeat-x scrolling-background-2"> </div>
+        <div className="backgroundLayer bg-[url(/BARS.png)] scrolling-background-2 h-12 -translate-y-[23px]"/>
       </div>
 
-      {/* Rocky section parallax */}
+      {/* Rocky section vertical parallax */}
       <div className="relative  h-screen bg-[#2b60c9]">
         <Parallax speed={-10} className="absolute inset-0">
-          <div className="-translate-y-[-50px] w-full h-screen bg-[url(/BG_7.png)] bg-[length:1500px_auto] bg-bottom bg-repeat-x bg-scroll" />
+          <div className="backgroundLayer bg-[url(/BG_7.png)] -translate-y-[-50px]"/>
         </Parallax>
 
         <Parallax speed={5} className="absolute inset-0">
-          <div className="-translate-y-[-50px] w-full h-screen bg-[url(/BG_8_1.png)] bg-[length:1500px_auto] bg-bottom bg-repeat-x bg-scroll" />
+          <div className="backgroundLayer bg-[url(/BG_8_1.png)] -translate-y-[-50px]"/>
         </Parallax>
       </div>
 
       {/* Rocky info parallax */}
       <div className="relative  h-screen bg-[#2b60c9]">
-        <div className="absolute inset-0 bg-[url(/BG_8.png)] bg-size-[1500px_auto] bg-repeat bg-top"></div>
+        <div className="absolute inset-0 bg-[url(/BG_8.png)] bg-size-[1500px_auto] bg-repeat bg-top"/>
       </div>
 
       {/* Grey section parallax */}
       <div className="relative h-screen bg-[#484868]">
         {/* Scrolling bar */}
-        <div className="-translate-y-[23px] w-full h-12 bg-[url(/BARS.png)] bg-size-[1500px_auto] bg-repeat-x scrolling-background-2"> </div>
+        <div className="backgroundLayer bg-[url(/BARS.png)] scrolling-background-2 h-12 -translate-y-[23px]"/>
       </div>
     </div>
   )
