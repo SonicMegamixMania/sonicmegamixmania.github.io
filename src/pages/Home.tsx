cd35b1ba3da3ab81ba00d03ef9c9e696e52b0f32
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-12-28 01:44:25 -0300

    Story Text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,33 +3,14 @@ import Lenis from '@studio-freight/lenis'
 import { useEffect } from 'react'
 import 'animate.css';
 
+import LiteYouTubeEmbed from 'react-lite-youtube-embed';
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
 
 import sonicMegamixLogo from "../assets/logo.png"
 
  const Home = () => {
-  {/* Lenis (smooth mouse scroll) configuration */}
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.1,
-      wheelMultiplier: 1,
-      touchMultiplier: 2,
-      syncTouch: true,
-      smoothWheel: true,
-      infinite: false,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    })
-
-    function raf(time: number) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-
-    requestAnimationFrame(raf)
-    return () => lenis.destroy()
-  }, [])
-
   return (
-    <div className=" m-auto">
+    <div className="m-auto text-white font-outfit 2xl:text-2xl">
       <div className="relative w-full h-screen bg-fixed overflow-hidden bg-[#1938cf]">
         {/* Hill and water backgrounds */}
         <div className="backgroundLayer bg-[url(/BG_1.png)] scrolling-background-1"/>
@@ -64,9 +45,69 @@ import sonicMegamixLogo from "../assets/logo.png"
       </div>
 
       {/* Blue info section */}
-      <div className="relative h-screen bg-[#2b60c9]">
+      <div className="relative min-h-screen h-full bg-[#2b60c9]">
         {/* Scrolling bar */}
         <div className="backgroundLayer bg-[url(/BARS.png)] scrolling-background-2 h-12 -translate-y-[23px]"/>
+        
+        {/* YouTube Video and Text Two Columns*/}
+        <div className="relative z-50 pt-60 pl-5 pr-5 m-auto max-w-[1750px] 
+                        flex flex-row items-center">
+          <div className="w-full">
+            <LiteYouTubeEmbed
+              id="vY0y033-Gic"
+              title="Rick Astley - Never Gonna Give You Up"
+            />
+          </div>
+
+          <div className="w-full pl-5">
+              <p className="text-justify">
+                <span className="text-4xl uppercase">Sonic Megamix Mania</span> is a fan-made modification for Sonic Mania Plus that aims to carry the torch of the discontinued ROM hack known as “Sonic the Hedgehog Megamix”. Megamix Mode adapts concepts from the ROM hack combined with the team's own vision bringing a unique experience built upon the foundation of the original. Classic Mode is a faithful remake of the content from “V.4.0b” of the ROM hack that offers a familiar experience with a fresh coat of paint. Sonic Megamix Mania offers beginner friendly and challenging experiences for old and new fans alike.<br/><br/>We plan to offer future updates to the project so follow our socials and be sure to stay tuned!
+              </p>
+          </div>
+        </div>
+
+        {/* Story Text*/}
+        <div className="relative z-50 pt-60 pl-5 pr-5 m-auto max-w-[1750px] 
+                        flex flex-row items-center">
+          <div className="mr-auto ml-auto pl-5 w-full max-w-[875px]">
+              <p className="text-justify">
+                <span className="text-4xl uppercase">5 months have passed</span> since Sonic the Hedgehog's previous plight against the nefarious Dr. Eggman. Ever since their first conflict the two sides have been battling constantly, with victory being just out of Eggman's grasp. However, after years of fighting Eggman had vanished after the incident on Space Colony Ark, leaving the world safely in Sonic’s hands - or so it was thought.
+              </p>
+          </div>
+        </div>
+        <div className="relative z-50 pt-30 pl-5 pr-5 m-auto max-w-[1750px] 
+                        flex flex-row items-center">
+          <div className="w-full">
+          </div>
+
+          <div className="w-full pl-5">
+              <p className="text-justify">
+                <span className="text-4xl uppercase">During this time of peace</span> the Chaotix discover a paper trail of blueprints to be built on an Island known as the Chiral Peninsula. These plans have Eggman’s name written all over them, requiring the power source of the Chaos Rings or Master Emerald. In his solidarity, Eggman was able to make a ton of breakthroughs using his research from Newtrogic Island. Now he is able to create robots that use positive ring energy as their power source, helping him stay under the radar of Sonic and G.U.N. 
+              </p>
+          </div>
+        </div>
+        <div className="relative z-50 pt-30 pl-5 pr-5 m-auto max-w-[1750px] 
+                        flex flex-row items-center">
+          <div className="w-full">
+              <p className="text-justify">
+                <span className="text-4xl uppercase">With this information in hand</span>, Mighty calls upon his good friend Ray to warn Sonic the Hedgehog and Knuckles the Echidna about their lead on Eggman. In this rare time of freedom, the flickies are able to repay their debt to Sonic after the events on Flicky Island. They prevent Eggman from using the Chaos rings, breaking them down into a plethora of blue, red, and yellow rings scattered throughout dimensions. 
+              </p>
+          </div>
+
+          <div className="w-full pl-5">
+          </div>
+        </div>
+        <div className="relative z-50 pt-30 pl-5 pr-5 m-auto max-w-[1750px] 
+                        flex flex-row items-center">
+          <div className="w-full">
+          </div>
+
+          <div className="w-full pl-5">
+              <p className="text-justify">
+                <span className="text-4xl uppercase">Without hesitation</span>, Sonic and friends travel to the Chiral Peninsula. As Ray drops them off one by one, he discovers an unconscious figure that looks similar to Sonic. Unbeknownst to Ray, he reawakens Shadow the Hedgehog. Without any recollection of his past, Ray decides to drop him off at the peninsula but as they approach, Shadow spots Eggman and orders Ray to chase him down. While it may just be intuition, Shadow feels the need to reclaim a sense of vengeance and recover his memories.”
+              </p>
+          </div>
+        </div>
       </div>
 
       {/* Rocky section vertical parallax */}
