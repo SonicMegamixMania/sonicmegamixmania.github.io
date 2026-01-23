9269d41272b85f9aae63528de34618627c7dbb1f
Gabriel Casanova <con.casanovaproductions@outlook.com>
2026-01-23 16:24:12 -0300

    Added discord and favicon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -416,11 +416,13 @@ const Home = () => {
 
           <p className='mt-10 text-4xl'><Link className='underline' to="/Credits">Full Credits</Link></p>
           <p className='m-5'>
-            <a className='underline' href='https://twitter.com/MegamixMania'>Twitter</a>
+            <a className='underline' target='_blank' href='https://twitter.com/MegamixMania'>Twitter</a>
             <span className='ml-2 mr-2'>&bull;</span>
-            <a className='underline' href='https://bsky.app/profile/megamixmania.bsky.social'>BlueSky</a>
+            <a className='underline' target='_blank' href='https://bsky.app/profile/megamixmania.bsky.social'>BlueSky</a>
             <span className='ml-2 mr-2'>&bull;</span>
-            <a className='underline' href='https://www.youtube.com/@sonicmegamixmania'>YouTube</a>
+            <a className='underline' target='_blank' href='https://www.youtube.com/@sonicmegamixmania'>YouTube</a>
+            <span className='ml-2 mr-2'>&bull;</span>
+            <a className='underline' target='_blank' href='https://discord.com/servers/sonic-megamix-mania-community-server-493217624763924481'>Discord</a>
           </p>
         </div>
       </div>
